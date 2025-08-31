adbd2478d86cb647e29fd4e0d92bbe450b44b95a
bipna01 <srmabpna01@gmail.com>
2025-08-31 14:58:23 -0700

    datatypesize

---
+++ DataType.java
@@ -0,0 +1,24 @@
+public class DataType{
+    public static void main( String[]args){
+        int number = 1234567890;
+        char charVariable ='h';
+        String stringvariable ="hello";
+        long longvariable =12345678900L;
+        short shortvariable =12345;
+        byte bytevariable = (byte)344;
+        float temperature =36.667f;
+        double gravity =9.80665;
+        int binary =0b1010;
+        int octal =012;
+        int hex = 0XA;
+        byte small =(byte)127;
+        char grade ='A'; 
+        boolean isValid = true;
+        String message ="Literals demo";
+        System.out.println("a");
+
+
+
+        
+    }
+}
