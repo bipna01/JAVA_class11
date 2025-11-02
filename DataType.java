c3650c172aa79fc5d8b47aad5445c1faa175ce79
bipna01 <srmabpna01@gmail.com>
2025-11-02 13:58:02 -0800

    ifelse

--- DataType.java
+++ DataType.java
@@ -5,7 +5,7 @@ public class DataType{
         String stringvariable ="hello";
         long longvariable =12345678900L;
         short shortvariable =12345;
-        byte bytevariable = (byte)344;
+        byte bytevariable bytevariable = (byte)344;
         float temperature =36.667f;
         double gravity =9.80665;
         int binary =0b1010;
@@ -15,7 +15,19 @@ public class DataType{
         char grade ='A'; 
         boolean isValid = true;
         String message ="Literals demo";
-        System.out.println("a");
+        System.out.println("int says: I can hold the big data like " + number);
+        System.out.println( "char say: i am just lonely character" + charVariable);
+        System.out.println(" String say:"+ stringvariable +"is my fav word");
+        System.out.println(" long say: i am huge example" + longvariable);
+        System.out.println("short say:dont understimate me i can hold" + shortvariable);
+        System.out.println(" byte say:i tried to hold " + bytevariable  );
+         System.out.println(" float say: human bodt temperature i about" + temperature);
+         System.out.println(" double say : gravity of earth is" + gravity);
+         System.out.println(" boolen say: is it valid?" + isValid);
+         
+
+
+
 
 
 
