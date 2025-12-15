c72b03fc0d3f15a3991e3258a1fcc5410f441466
Bipana Sharma <srmabpna01@gmail.com>
2025-12-15 21:02:22 +0545

    number guess

--- Guess.java
+++ Guess.java
@@ -9,13 +9,13 @@ public class Guess{
 
      void guessNum(){
          Scanner input= new Scanner(System.in);
-         int count=0;
+        
         while(true) {
              System.out.println("Enter the number you want to guess");
        int num = input.nextInt();
 
             
-        count++;
+        
        if (num > secretNumber) {
     System.out.println("Your number is greater than my secret number");
 }
@@ -24,8 +24,7 @@ else if (num < secretNumber) {
 }
 else {
     System.out.println("Your guess is correct");
-    System.out.println("You guessed the number in " + count + " guesses.");
-                break;   // stop l
+    
 }
 
     }
