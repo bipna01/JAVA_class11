d11df7544b3b0e156cc99e2b48681861f82f9346
bipna01 <srmabpna01@gmail.com>
2026-02-22 15:04:05 +0545

    Aray

---
+++ ClassAndObject/Array/Customer.java
@@ -0,0 +1,28 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Customer{
+    public static void main(){
+        List<Customer> customerRoster = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
+         boolean choice = true;
+
+        while (choice) {
+
+            System.out.println("Enter the name:");
+            String name = input.nextLine();
+
+            System.out.println("Enter the number :");
+            int number = input.nextInt();
+
+            input.nextLine(); 
+
+            CustomerRoster.add(new Customer(name,));
+        String name;
+
+
+    }
+
+}
